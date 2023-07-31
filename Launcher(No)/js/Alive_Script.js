e8f5cc655841f38146957413c59b1bc095be245d
Gost-ver <93863775+Gost-ver@users.noreply.github.com>
2023-07-31 15:27:30 +0300

    Alive_Script.js

--- Launcher(No)/js/Alive_Script.js
+++ Launcher(No)/js/Alive_Script.js
@@ -132,7 +132,6 @@ SubscribePoint__Btn.forEach(function(I_Btn){
                 }
                 else{
                     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-                    window.close();
                 }
             }
         }
