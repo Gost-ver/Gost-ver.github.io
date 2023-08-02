e94cf0e9cbd1601df553e12af321107619d1d72c
Gost-ver <93863775+Gost-ver@users.noreply.github.com>
2023-08-02 19:49:02 +0300

    general.js

--- html_inner(ForGit)/js_inner/general.js
+++ html_inner(ForGit)/js_inner/general.js
@@ -4,7 +4,7 @@ Ava.addEventListener("click", function(){
     if(TipoInput.value == "plague"){
         LinkBtn.setAttribute("href", "https://www.youtube.com/watch?v=482tDopNzoc");
     }
-    if(TipoInput.value == "Чёрт"){
+    if(TipoInput.value == "Chert"){
         LinkBtn.setAttribute("href", "https://www.youtube.com/watch?v=zmmICPynDXI");
     }
     LinkBtn.setAttribute("target", "_blank");
